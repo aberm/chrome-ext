659b5498966f22a179f45f5247c5cc4a95d9bea7
aberm <abermendel@gmail.com>
2019-07-09 14:06:02 -0400

    update

--- menu.js
+++ menu.js
@@ -6,10 +6,6 @@ const openNewTab = (element, tab) => {
 
 openNewTab("view", "collection.html");
 openNewTab("how", "how.html");
-openNewTab(
-  "shmuella",
-  "https://media.licdn.com/dms/image/C5603AQHi7OD80ugWTw/profile-displayphoto-shrink_800_800/0?e=1564617600&v=beta&t=bnS1H9va8K6WZjBvFYuHNLmPxM1iImWI9MNcsNV3V_U"
-);
 
 document.getElementById("add").addEventListener("click", e => {
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
