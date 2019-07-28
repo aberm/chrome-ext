b465b392f5b9fb49a2dc63a44f57c8818ee4b543
aberm <abermendel@gmail.com>
2019-07-28 19:31:22 -0400

    add product title into popup

--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
+import Scraper from "./scraper.js";
+
 let activeTab;
 
 chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
   activeTab = tabs[0];
+
+  const x = new Scraper(activeTab.url);
+  x.scrape().then(res => {
+    document.getElementById("name").innerText = res.title;
+  });
 });
 
 document.getElementById("add").addEventListener("click", e => {
