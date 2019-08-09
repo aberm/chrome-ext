194604592927658fb79a74ac4f5e0572768d6baf
aberm <abermendel@gmail.com>
2019-08-09 12:18:14 -0400

    reconstructed scraper functions

--- scraper.js
+++ scraper.js
@@ -1,255 +1,192 @@
 class Scraper {
-  constructor(url, debugMode = false) {
+  constructor(url, doc, debugMode = false) {
     this.url = url;
+    this.doc = doc;
     this.debugMode = debugMode;
   }
 
   scrape = async () => {
-    return await this.unpackUrl(this.url);
-  };
-
-  /**
-   * Pure function that returns JS Object with scraped metadata values.
-   */
-  unpackUrl = async url => {
-    return await this.fetchAndParseUrl(url).then(htmlDocument => {
-      return this.scrapeInfo(htmlDocument, url);
-    });
+    const res = await this.fetchAndParseUrl(this.url);
+    this.doc = res;
+    return this.scrapeInfo();
   };
 
   /**
    * fetch and parse a given url and return HTML Document
    */
-  fetchAndParseUrl = url => {
+  fetchAndParseUrl = async url => {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    return fetch(proxyurl + url).then(res => {
-      if (res.ok) {
-        return res.text().then(site => {
-          const parser = new DOMParser();
-          const htmlDocument = parser.parseFromString(site, "text/html");
-          return htmlDocument;
-        });
-      }
-    });
+    const res = await fetch(proxyurl + url);
+    const site = await res.text();
+    const parser = new DOMParser();
+    return parser.parseFromString(site, "text/html");
   };
 
   /**
    * Scrape metadata from given HTML Document and return JS Object of data
    */
-  scrapeInfo = (site, url) => {
+  scrapeInfo = () => {
+    if (this.doc === undefined) {
+      console.error("fetch failed: " + this.url);
+      return;
+    }
     const data = {
-      url: url,
-      title: this.scrapeField("name", site).length
-        ? this.scrapeField("name", site)[0]
-        : site.title,
-      image: this.imagePrependHttp(this.scrapeField("image", site)[0]),
-      description: this.decodeHtmlEntities(
-        this.scrapeField("description", site)[0]
-      ).trim(),
-      price:
-        this.priceRemoveCommas(this.scrapeField("price", site)[0]) ||
-        this.priceRemoveCommas(this.scrapeField("price:amount", site)[0])
+      url: this.url,
+      title: this.getTitle(),
+      image: this.getImage(),
+      description: this.getDescription(),
+      price: this.getPrice()
     };
 
     return data;
   };
 
-  /**
-   * Rules for scraping HTML Document to find metadata. Returns array of found data.
-   */
-  scrapeField = (field, doc) => {
-    // for the weird Javascript nested scope variable bindings:
-    const jsonFieldFinder = this.jsonFieldFinder;
-    const debugMode = this.debugMode;
-
-    let rules = [
-      function() {
-        const results = doc.querySelectorAll('[itemprop="' + field + '"]');
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ['[itemprop="' + field + '"]']: [...results].map(
-                  el => el.content
-                )
-              })
-            : null;
-          return results[0].content;
-        }
-      },
-      function() {
-        const results = doc.querySelectorAll(
-          'meta[property="og:' + field + '"]'
-        );
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ['meta[property="og:' + field + '"]']: [...results].map(
-                  el => el.content
-                )
-              })
-            : null;
-          return results[0].content;
-        }
-      },
-      function() {
-        const results = doc.querySelectorAll(
-          'meta[property="twitter:' + field + '"]'
-        );
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ['meta[property="twitter:' + field + '"]']: [...results].map(
-                  el => el.content
-                )
-              })
-            : null;
-          return results[0].content;
-        }
-      },
-      function() {
-        const results = doc.querySelectorAll('meta[name="og:' + field + '"]');
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ['meta[name="og:' + field + '"]']: [...results].map(
-                  el => el.content
-                )
-              })
-            : null;
-          return results[0].content;
-        }
-      },
-      function() {
-        const results = doc.querySelectorAll(
-          'meta[name="twitter:' + field + '"]'
-        );
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ['meta[name="twitter:' + field + '"]']: [...results].map(
-                  el => el.content
-                )
-              })
-            : null;
-          return results[0].content;
-        }
-      },
-      function() {
-        const results = doc.querySelectorAll('meta[itemprop="' + field + '"]');
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ['meta[itemprop="' + field + '"]']: [...results].map(
-                  el => el.content
-                )
-              })
-            : null;
-          return results[0].content;
-        }
-      },
-      function() {
-        const results = doc.querySelectorAll('meta[name="' + field + '"]');
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ['meta[name="' + field + '"]']: [...results].map(
-                  el => el.content
-                )
-              })
-            : null;
-          return results[0].content;
-        }
-      },
-      function() {
-        const results = doc.querySelectorAll('span[itemprop="' + field + '"]');
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ['span[itemprop="' + field + '"]']: [...results].map(el =>
-                  el.getAttribute("content")
-                )
-              })
-            : null;
-          return results[0].getAttribute("content");
-        }
-      },
-      function() {
-        if (doc.querySelectorAll('script[type="application/ld+json"]').length) {
-          const arr = [];
-          doc
-            .querySelectorAll('script[type="application/ld+json"]')
-            .forEach(json => {
-              const a = [];
-
-              jsonFieldFinder(JSON.parse(json.innerText), field, a);
-              arr.push(a);
-            });
-          debugMode
-            ? console.log({
-                'script[type="application/ld+json"]': arr
-                  .filter(item => item.length)
-                  .flat()
-              })
-            : null;
-          return arr.filter(item => item.length);
-        }
+  getTitle = () => {
+    const s = () => {
+      const results = this.doc.querySelectorAll("h1");
+      if (results.length) {
+        this.debugMode
+          ? console.log({
+              ["h1"]: [...results].map(el => el.innerText.trim())
+            })
+          : null;
+        return [...results].map(el => el.innerText.trim());
       }
+    };
+
+    return this.mostCommonItem([
+      ...(this.listRules("title") || [this.doc.title]),
+      s(),
+      ...this.jsonFieldScraper("title")
+    ]);
+  };
+
+  getImage = () => {
+    const s = () => {
+      const title = this.getTitle(this.doc);
+      const results = this.doc.querySelectorAll('img[alt*="' + title + '"]');
+      if (results.length) {
+        this.debugMode
+          ? console.log({
+              ['img[alt*="' + title + '"]']: [...results].map(el => el.src)
+            })
+          : null;
+        return [...results].map(el => el.src);
+      }
+    };
+
+    return this.imagePrependHttp(
+      this.mostCommonItem([
+        ...this.listRules("image"),
+        s(),
+        ...this.jsonFieldScraper("image")
+      ])
+    );
+  };
+
+  getDescription = () => {
+    const desc = this.mostCommonItem([
+      ...this.listRules("description"),
+      ...this.jsonFieldScraper("description")
+    ]);
+
+    return desc === undefined
+      ? undefined
+      : this.decodeHtmlEntities(desc).trim();
+  };
+
+  getPrice = () => {
+    const s = () => {
+      const results = this.doc.querySelectorAll('span[id*="price"]');
+      if (results.length) {
+        this.debugMode
+          ? console.log({
+              ['span[id*="price"]']: [...results].map(el => el.innerText.trim())
+            })
+          : null;
+        return [...results].map(el => el.innerText.trim());
+      }
+    };
+
+    return this.priceRemoveCommasAnd$(
+      this.mostCommonItem([
+        ...(this.listRules("price") || this.listRules("price:amount")),
+        s(),
+        ...(this.jsonFieldScraper("price") ||
+          this.jsonFieldScraper("price:amount"))
+      ])
+    );
+  };
+
+  singleRuleScraper = (field, output, attribute = "", tag = "", seo = "") => {
+    const results = this.doc.querySelectorAll(
+      `${tag}[${attribute}="${seo}${field}"]`
+    );
+    if (results.length) {
+      this.debugMode
+        ? console.log({
+            [`${tag}[${attribute}="${seo}${field}"]`]: [...results].map(
+              el => el[output]
+            )
+          })
+        : null;
+
+      return [...results].map(z => z.getAttribute(output));
+    }
+  };
+
+  listRules = field => {
+    const list = [
+      ["content", "itemprop"],
+      ["content", "property", "meta", "og:"],
+      ["content", "property", "meta", "twitter:"],
+      ["content", "name", "meta", "og:"],
+      ["content", "name", "meta", "twitter:"],
+      ["content", "itemprop", "meta"],
+      ["content", "name", "meta"],
+      ["content", "itemprop", "span"]
     ];
 
-    if (field === "image") {
-      rules.push(function() {
-        if (doc.querySelectorAll('img[itemprop="image"]').length) {
-          return doc.querySelectorAll('img[itemprop="image"]')[0].src;
-        }
-      });
-    }
-    if (field === "name") {
-      // rules = rules.reverse();
-      rules.push(function() {
-        const results = doc.querySelectorAll("h1");
-        if (results.length) {
-          debugMode
-            ? console.log({
-                ["h1"]: [...results].map(el => el.innerText.trim())
-              })
-            : null;
-          return results[0].innerText.trim();
-        }
-      });
+    switch (field) {
+      case "image":
+        list.push(["src", "itemprop", "img"]);
+        break;
     }
 
-    /* sorts for majority */
+    return list.map(rule => this.singleRuleScraper(field, ...rule));
+  };
 
-    const flatArray = rules
-      .map(rule => rule())
-      .filter(rule => rule)
-      .flat()
-      .flat()
-      .flat();
+  jsonFieldScraper = field => {
+    if (
+      this.doc.querySelectorAll('script[type="application/ld+json"]').length
+    ) {
+      const arr = [];
+      this.doc
+        .querySelectorAll('script[type="application/ld+json"]')
+        .forEach(json => {
+          const a = [];
 
-    const cnts = flatArray.reduce((obj, val) => {
-      obj[val] = (obj[val] || 0) + 1;
-      return obj;
-    }, {});
-
-    const sorted = Object.keys(cnts).sort((a, b) => {
-      return cnts[b] - cnts[a];
-    });
-
-    // return rules
-    //   .map(rule => rule())
-    //   .filter(rule => rule)
-    //   .flat()
-    //   .flat();
-
-    return sorted;
+          this.jsonFieldSearcher(JSON.parse(json.innerText), field, a);
+          arr.push(a);
+        });
+      this.debugMode
+        ? console.log({
+            'script[type="application/ld+json"]': arr
+              .filter(item => item.length)
+              .flat()
+          })
+        : null;
+      return arr.filter(item => item.length);
+    } else {
+      return [];
+    }
   };
 
   /**
    * Function for scraping HTML Document to find LD-JSON metadata.
    * Returns array of found data.
    */
-  jsonFieldFinder = (json, field, array) => {
+  jsonFieldSearcher = (json, field, array) => {
     // if (array.length < 1) {
     // comment this line to get multiple results
     if (
@@ -262,10 +199,28 @@ class Scraper {
     } else {
       for (let i = 0; i < Object.keys(json).length; i++) {
         if (typeof json[Object.keys(json)[i]] == "object") {
-          this.jsonFieldFinder(json[Object.keys(json)[i]], field, array);
+          this.jsonFieldSearcher(json[Object.keys(json)[i]], field, array);
         }
       }
     }
+  };
+
+  mostCommonItem = arr => {
+    const cnts = arr
+      .filter(x => x) // remove undefined, null
+      .flat()
+      .flat()
+      .flat()
+      .reduce((obj, val) => {
+        obj[val] = (obj[val] || 0) + 1;
+        return obj;
+      }, {});
+
+    const sorted = Object.keys(cnts).sort((a, b) => {
+      return cnts[b] - cnts[a];
+    });
+
+    return sorted[0];
   };
 
   decodeHtmlEntities = str => {
